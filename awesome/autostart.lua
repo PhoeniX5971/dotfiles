61e06b60b56ef29a0cdb7e1ed80336e25ebca1ba
PhoeniX5971 <moh845195@gmail.com>
2025-04-26 00:32:18 +0300

    ~

--- awesome/autostart.lua
+++ awesome/autostart.lua
@@ -3,5 +3,8 @@ local awful = require("awful")
 -- Start polkit authentication agent
 awful.spawn.with_shell("/usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1")
 
+-- Bind Caps to Esc
+awful.spawn.with_shell("setxkbmap -option caps:escape")
+
 -- Start picom (with your config file)
 awful.spawn.with_shell("picom")
